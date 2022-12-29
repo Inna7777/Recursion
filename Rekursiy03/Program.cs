76a5505c340efd94d7ec7397b315e29c89194036
Inna7777 <peshkova-inna@mail.ru>
2022-12-29 17:57:12 +0400

    Add the third rekursiy

---
+++ Rekursiy03/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии.
+int FunAkkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+      if ((m > 0) && (n == 0))
+        return FunAkkerman(m - 1, 1);
+    else
+        return FunAkkerman(m - 1, FunAkkerman(m, n - 1));
+}
+ 
+Console.WriteLine(FunAkkerman(3, 2)); 
+Console.WriteLine(FunAkkerman(2, 3)); 
+ 
+Console.WriteLine();
