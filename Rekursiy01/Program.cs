0edba6318c23c35330b66d55a89f4f7974190831
Inna7777 <peshkova-inna@mail.ru>
2022-12-29 17:48:40 +0400

    Add thr first rekursiy

---
+++ Rekursiy01/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте значение N. Напишите программу, которая выведет
+ //все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+void NaturalNumber(int N)
+{    
+       if (N == 0) return ; 
+       Console.Write(N+" ");
+       NaturalNumber(N-1);
+        
+}
+int N = GetNumber("Введите число:");
+NaturalNumber(N);
+Console.WriteLine();
